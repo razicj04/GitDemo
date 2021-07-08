3ad5414d81105ad5577d39a270ba5f74944a8e89
razicj04 <razicj04@yahoo.com>
2021-07-08 23:26:38 +0800

    commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testing xml file from maven
+	//how to execute all test cases from Test folder - mvn test
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+	}
+
+}
